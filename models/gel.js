29e5cc97b4c490ec1d765950ac54036890950872
rakeshmerugu <112118622+rakeshmerugu@users.noreply.github.com>
2022-11-30 14:11:47 -0600

    added

--- models/gel.js
+++ models/gel.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose")
 const gelSchema = mongoose.Schema({
-    Gel_Name: String,
-    Gel_Company: String,
-    Gel_Size: Number,
-    Gel_Rating: Number
+    Gel_Name: {type: String,required: [true, 'Name of the Gel cannot be empty']},
+    Gel_Company: {type: String,required: [true,'Company of the Gel cannot be empty']},
+    Gel_Size:{type: String,required:[true,'Size of the Gel cannot be empty']},
+    Gel_Rating:{type: String,required:[true,'Rating of the Gel cannot be empty']}
 })
 
 module.exports = mongoose.model("Gel",
